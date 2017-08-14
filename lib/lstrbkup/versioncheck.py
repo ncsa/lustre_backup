27d3beb55fa9329e2771d73f328ca0e378c150a0
David Raila <raila@illinois.edu>
2017-08-14 15:37:56 -0500

    log vers errs

--- lib/lstrbkup/versioncheck.py
+++ lib/lstrbkup/versioncheck.py
@@ -2,11 +2,12 @@
 #
 # use at startup to check, re-exec under scl python if needed
 #
-import os
 import sys
+import logging
 
 VERSION_WANTED = 27
 vers = int(sys.version_info[0])*10 + int(sys.version_info[1])
 if vers < VERSION_WANTED:
+  logging.critical('*********** WRONG PYTHON VERSION: {} for {}'.format(vers, sys.argv))
   print '*********** WRONG PYTHON VERSION: {}'.format(vers)
-  os.exit(-27)
+  sys.exit(-27)
