fcd577786e26ee332c7583514559a9a6bf5054f3
David Raila <raila@illinois.edu>
2017-08-15 12:18:29 -0500

    update updater

---
+++ bin/update_lustre_backup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+BASE=/mnt/a/settools/lustrebackup
+
+host=$( hostname )
+matches=$( echo $host | grep '^ie' | wc -l )
+if [ $matches -ne 1 ]; then
+  echo "This host, '$host', does not appear to be an ie node.  Exiting."
+  #exit 1
+fi
+
+#
+# Doublecheck the python version, re-exec under scl if necessary
+#
+PYVERS=$(python --version 2>&1 | cut -d' ' -f2)
+[[ $PYVERS > '2.7.0' ]] || exec scl enable python27 $0
+
+easy_install globus-sdk rpyc ptvsd
+
+service lustre_backup_manager stop
+service lustre_backup_service stop
+
+
+mkdir -p $BASE || true
+src_fn="$BASE/bin/lustre_backup.rc"
+for fn in lustre_backup_manager lustre_backup_service; do
+  tgt_fn="/etc/init.d/$fn"
+  set -x
+  rm -f "$tgt_fn"
+  ln -s "$src_fn" "$tgt_fn"
+  set +x
+done
+
+#/usr/bin/easy_install "globusonline-transfer-api-client==0.10.16"
+
+/sbin/sysctl -w kernel.shmmax=68719476736
+
+chckonfig lustre_backup_service on
+chckonfig lustre_backup_manager on
+service lustre_backup_manager start
+service lustre_backup_service start
+
+echo "DONE"
+
+
+
+
+
